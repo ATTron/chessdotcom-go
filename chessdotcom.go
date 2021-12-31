035f681c6ce4a0cc47a539f8ceb2afd262b8b641
Anthony Templeton <anthony@templetron.io>
2021-12-30 21:52:29 -0500

    convert from if statement to switch

--- chessdotcom.go
+++ chessdotcom.go
@@ -10,7 +10,8 @@ import (
 )
 
 func processRespJSON(resp *http.Response) (string, error) {
-	if resp.StatusCode == 200 {
+	switch resp.StatusCode {
+	case 200:
 		b, _ := ioutil.ReadAll(resp.Body)
 		var jsonResp map[string]interface{}
 		err := json.Unmarshal(b, &jsonResp)
@@ -18,17 +19,18 @@ func processRespJSON(resp *http.Response) (string, error) {
 		returnResp, err := json.MarshalIndent(&jsonResp, "", "  ")
 		util.Check(err)
 		return string(returnResp), nil
-	} else if resp.StatusCode == 404 {
+	case 404:
 		log.Println("Unable to find the request you are looking for")
 		return "", util.ErrNotFound
-	} else {
+	default:
 		log.Fatal("Could not return valid response from server . . .")
 		return "", util.ErrBadType
 	}
 }
 
 func processRespArray(resp *http.Response) (string, error) {
-	if resp.StatusCode == 200 {
+	switch resp.StatusCode {
+	case 200:
 		b, _ := ioutil.ReadAll(resp.Body)
 		var jsonResp []interface{}
 		err := json.Unmarshal(b, &jsonResp)
@@ -36,10 +38,10 @@ func processRespArray(resp *http.Response) (string, error) {
 		returnResp, err := json.MarshalIndent(&jsonResp, "", "  ")
 		util.Check(err)
 		return string(returnResp), nil
-	} else if resp.StatusCode == 404 {
+	case 404:
 		log.Println("Unable to find the request you are looking for")
 		return "", util.ErrNotFound
-	} else {
+	default:
 		log.Fatal("Could not return valid response from server . . .")
 		return "", util.ErrBadType
 	}
